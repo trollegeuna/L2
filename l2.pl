0ab072401cd982a6104ef35bda9759ea42715219
Trolle <trolle.geuna@gmail.com>
2015-10-11 18:11:51 +0200

    snart så....

--- l2.pl
+++ l2.pl
@@ -12,10 +12,14 @@ spider(X):-
   person(X),
   % Hittar denna persons Kompisar
   allaMinaKamrater(X,Kompisar),
-  % Kolla Kolla
+  % Return lista med kamrater som inte känner varandra. Dvs möjliga konspiratorer.
   elimineringUtavKamrater(Kompisar,KonstigaManniskor),
-  write(KonstigaManniskor),
-  backa(X,Kompisar).
+
+  jagEPoppis(KonstigaManniskor).
+  %write(KonstigaManniskor)
+  % Backtrack, kolla detta för alla personer.
+  % backa(X,Kompisar).
+
 
 allaMinaKamrater(X, Kompisar):-
   bagof(Kamrat, kompis(X,Kamrat), Kompisar).
@@ -26,37 +30,32 @@ kompis(A,B) :- knows(A,B) ; knows(B,A).
 
  
 
-% % Känner någon i listan någon?
-% noFriends(P, []).
-%
-% noFriends(P1, [P2|Tail]):-
-%   not(hittaMinaKamrater(P,Hokus)),
-%   noFriends(Tail).
+
+% TODO FIXA kanskeJagGillarDig
+% Känner någon i listan någon?
+kanskeJagGillarDig(_, []).
+
+kanskeJagGillarDig(P1, [P2|Tail]):-
+  allaMinaKamrater(P1,_),
+  kanskeJagGillarDig(Tail).
 
 
 elimineringUtavKamrater([NarmasteKompisen|Kompisar], KonspirationsTeoretiker):-
   allaMinaKamrater(NarmasteKompisen, Svar),
   subtract(Kompisar, Svar, KollaIgen),
   elimineringUtavKamrater(KollaIgen,K1),
-  !,
   KonspirationsTeoretiker = [NarmasteKompisen | K1].
 
-elimineringUtavKamrater([_|Kompisar], K):-elimineringUtavKamrater(Kompisar,K).
+elimineringUtavKamrater([_|Kompisar], K):-
+  elimineringUtavKamrater(Kompisar,K).
 
 elimineringUtavKamrater([],[]).
 
-% Return true om P inte känner någon i listan.
-% Return false om P känner någon i listan.
-checkInList(P, [P2|[]]):-
-  not(kompis(P,P2)).
 
-checkInList(P, [P2|List]):-
-  not(kompis(P,P2)),
-  checkInList(P, List).
-
-
-% Det här kommer inte gå!
-backa([X,Xs|Tail],K):-!.
+% TODO: FIXA forall
+jagEPoppis([KonstigaManniskor]):-
+  forall(Person(Asome),
+  member(Asome,KonstigaManniskor);kanskeJagGillarDig(Asome,KonstigaManniskor)).
 
 
 % hämta person X
