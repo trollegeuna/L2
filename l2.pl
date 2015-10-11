990d26676bfb127a513dd906c168a1ceeba38928
oskarcasselryd <oskarcasselryd@gmail.com>
2015-10-11 16:36:40 +0200

    asdasd

--- l2.pl
+++ l2.pl
@@ -1,5 +1,11 @@
 % By Trolle Geuna & Oskar Casselryd, 2015
 
+% Ta en person, kollar dess vänner.
+% Ta bort alla vänner som är vänner med spindelns vänner.
+
+
+
+
 
 spider(X):-
   person(X),
@@ -14,26 +20,29 @@ hittaMinaKamrater(X, Kompisar):-
 % Vänskap går åt båda hållen.
 kompis(A,B) :- knows(A,B) ; knows(B,A).
 
+ 
+
+% % Känner någon i listan någon?
+% noFriends(P, []).
+%
+% noFriends(P1, [P2|Tail]):-
+%   not(hittaMinaKamrater(P,Hokus)),
+%   noFriends(Tail).
 
 
-% Känner någon av personerna i listan varandra?
 
-noFriends([],_).
-
-noFriends([P|Tail]):-
-  checkInList(P, Tail),
-  noFriends(Tail).
 
 % Return true om P inte känner någon i listan.
+% Return false om P känner någon i listan.
 checkInList(P, [P2|[]]):-
-  kompis(P,P2).
+  not(kompis(P,P2)).
 
-  checkInList(P, [P2|List]):-
-  !,
-  \+ kompis(P,P2),
+checkInList(P, [P2|List]):-
+  not(kompis(P,P2)),
   checkInList(P, List).
 
 
+% Det här kommer inte gå!
 backa([X,Xs|Tail],K):-!.
 
 
