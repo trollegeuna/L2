5a1e829a0b4fcf48881d00643967fc6ba0a8986a
oskarcasselryd <oskarcasselryd@gmail.com>
2015-10-11 14:16:36 +0200

    början

--- l2.pl
+++ l2.pl
@@ -1,6 +1,27 @@
-
-spider(X):-
-
+% By Trolle Geuna & Oskar Casselryd, 2015
 
 
-kompis(X,Y) :- knows(A,B) ; knows(B,A).
+%spider(X):-
+
+
+% Vänskap går åt båda hållen.
+kompis(A,B) :- knows(A,B) ; knows(B,A).
+
+
+
+% Känner någon av personerna i listan varandra?
+
+noFriends([],_).
+
+noFriends([P|Tail]):-
+  checkInList(P, Tail),
+  noFriends(Tail).
+
+
+checkInList(P, [P2|[]]):-
+  kompis(P,P2).
+
+  checkInList(P, [P2|List]):-
+  !,
+  \+ kompis(P,P2),
+  checkInList(P, List).
