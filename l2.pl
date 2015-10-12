59b72e519de3ac8ddcba23fd41b47a6e53665e3a
oskarcasselryd <oskarcasselryd@gmail.com>
2015-10-12 20:13:46 +0200

    Vi är rätt nära nu. La till lite mer kommentarer

--- l2.pl
+++ l2.pl
@@ -14,7 +14,7 @@ spider(X):-
   allaMinaKamrater(X,Kompisar),
   % Return lista med kamrater som inte känner varandra. Dvs möjliga konspiratorer.
   elimineringUtavKamrater(Kompisar,KonstigaManniskor),
-
+  % Kolla om alla personer är vän med någon konspiratör. Om så är fallet är X spindel.
   jagEPoppis(KonstigaManniskor).
   %write(KonstigaManniskor)
   % Backtrack, kolla detta för alla personer.
@@ -29,8 +29,6 @@ allaMinaKamrater(X, Kompisar):-
 kompis(A,B) :- knows(A,B) ; knows(B,A).
 
  
-
-<<<<<<< HEAD
 % % Känner någon i listan någon?
 % noFriends(P, []).
 %
@@ -48,7 +46,8 @@ kanskeJagGillarDig(P1, [P2|Tail]):-
   allaMinaKamrater(P1,_),
   kanskeJagGillarDig(Tail).
 
-
+% TODO här kan vi ev. få fel när vi subtractar folk från listan.
+% TODO vi borde skicka in 2 listor. En med alla spindelns vänner som vi inte ändrar, och en där vi tar bort deras vänner.
 elimineringUtavKamrater([NarmasteKompisen|Kompisar], KonspirationsTeoretiker):-
   allaMinaKamrater(NarmasteKompisen, Svar),
   subtract(Kompisar, Svar, KollaIgen),
@@ -60,7 +59,8 @@ elimineringUtavKamrater([_|Kompisar], K):-
 
 elimineringUtavKamrater([],[]).
 
-
+% Kolla om alla personer känner någon i listan KonstigaManniskor.
+% Om detta är fallet returnera true. Annars false.
 % TODO: FIXA forall
 jagEPoppis([KonstigaManniskor]):-
   forall(Person(Asome),
