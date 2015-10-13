6865a71d0a94b07a635b95deeb797420723a09b7
oskarcasselryd <oskarcasselryd@gmail.com>
2015-10-13 12:18:45 +0200

    eliminering utav kamrater

--- l2.pl
+++ l2.pl
@@ -46,18 +46,24 @@ kanskeJagGillarDig(P1, [P2|Tail]):-
   allaMinaKamrater(P1,_),
   kanskeJagGillarDig(Tail).
 
+spindelns kompisar (A, B C D)
+As kompisar(B)
+spindels kompisar(C D)
+
+ C D
+
 % TODO här kan vi ev. få fel när vi subtractar folk från listan.
 % TODO vi borde skicka in 2 listor. En med alla spindelns vänner som vi inte ändrar, och en där vi tar bort deras vänner.
-elimineringUtavKamrater([NarmasteKompisen|Kompisar], KonspirationsTeoretiker):-
+elimineringUtavKamrater([NarmasteKompisen|Kompisar],KompisarKvar, KonspirationsTeoretiker):-
   allaMinaKamrater(NarmasteKompisen, Svar),
-  subtract(Kompisar, Svar, KollaIgen),
-  elimineringUtavKamrater(KollaIgen,K1),
+  subtract(Kompisar, Svar, KompisarKvar),
+  elimineringUtavKamrater(KollaIgen,Kompisar, KompisarKvar),
   KonspirationsTeoretiker = [NarmasteKompisen | K1].
 
-elimineringUtavKamrater([_|Kompisar], K):-
-  elimineringUtavKamrater(Kompisar,K).
+elimineringUtavKamrater([_|Kompisar], KompisarKvar, K):-
+  elimineringUtavKamrater(Kompisar,KompisarKvar, K).
 
-elimineringUtavKamrater([],[]).
+elimineringUtavKamrater([],[], []).
 
 % Kolla om alla personer känner någon i listan KonstigaManniskor.
 % Om detta är fallet returnera true. Annars false.
