8f21ca32d6955678beabfe3f74751fe6680f0c66
oskarcasselryd <oskarcasselryd@gmail.com>
2015-10-14 11:38:43 +0200

    Test

--- l2_examples/exampleX.pl
+++ l2_examples/exampleX.pl
@@ -1,12 +1,19 @@
-person(c1).
-person(o1).
-person(o2).
-person(o3).
+person(p1).
+person(p2).
+person(p3).
+person(p4).
+person(p5).
+person(p6).
+person(p7).
+person(p8).
 
-knows(c1, o1).
-knows(c1, o2).
-knows(c1, o3).
-
-knows(o1, o3).
-
-knows(o2, o3).
+knows(p1, p2).
+knows(p1, p8).
+knows(p3, p7).
+knows(p4, p8).
+knows(p5, p6).
+knows(p5, p7).
+knows(p5, p8).
+knows(p6, p7).
+knows(p6, p8).
+knows(p7, p8).
