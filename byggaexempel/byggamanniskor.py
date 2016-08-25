0cedaef767fa7822a5a9165454281c19e1e2fe01
Trolle <trolle.geuna@gmail.com>
2016-08-25 22:52:51 +0200

    slutet

--- byggaexempel/byggamanniskor.py
+++ byggaexempel/byggamanniskor.py
@@ -1,6 +1,7 @@
 
+
 def main():
-    writeToOutput(25)
+    writeToOutput(101)
 
 def writeToOutput(man):
      with open ("test.pl","w") as r:
@@ -8,13 +9,37 @@ def writeToOutput(man):
         while man > 0:
             r.write("person(p%s).\n" %man)
             man = man -1
-        x = 1
-        while (x < 2):
-            y = 1
-            while (y<20):
-                r.write("knows(p%s,p%s).\n" %(x,y))
-                y = y+1
-            x = x+1
+        r.write("\n")
+        r.write("\n")
+
+        b = 5
+        while (b < 101):
+            c = b -1
+            a = 4
+            while a > 0:
+                r.write("knows(p%s,p%s).\n" %(b,c))
+                c = c-1
+                a = a - 1
+            r.write("\n")
+            r.write("\n")
+            b = b+5
+        r.write("\n")
+        r.write("%""Spindeln i natet\n")
+        r.write("\n")
+        y = 5
+        while (y < 101) :
+            r.write("knows(p%s,p%s).\n" %(101,y))
+            y = y + 5
+
+
+
+        # x = 1
+        # while (x < 2):
+        #     y = 1
+        #     while (y<20):
+        #         r.write("knows(p%s,p%s).\n" %(x,y))
+        #         y = y+1
+        #     x = x+1
 
 
 
